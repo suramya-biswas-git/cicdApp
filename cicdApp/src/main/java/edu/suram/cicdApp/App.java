1b665c9f8d5742ef3b744cc4f3e389ecb23bbf8e
suramya-biswas-git <suramya.biswas@gmail.com>
2026-05-09 17:42:40 +0530

    first version

---
+++ cicdApp/src/main/java/edu/suram/cicdApp/App.java
@@ -0,0 +1,13 @@
+package edu.suram.cicdApp;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World This is CICD App!");
+    }
+}
+/*
+  https://github.com/suramya-biswas-git/cicdApp.git
+*/
